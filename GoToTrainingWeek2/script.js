43db8a898557b83a8fccffd0379e552115f43699
dexterellison <DexterLab@Dexters-MacBook-Pro.local>
2015-09-16 21:00:24 -0700

    My Work
    Classwork from this week

--- GoToTrainingWeek2/script.js
+++ GoToTrainingWeek2/script.js
@@ -1,10 +1,10 @@
 //Conditional Logic
 
-var oldEnough = true;
+var oldEnough = false;
 
 //if the child is old enough, print to the console "you can ride!"
 if(oldEnough){
     //code perform if condition is true
-console.log("You can ride the coaster!");
+    console.log("You can ride the coaster!");
 }
-
+console.log("what comes after");
---
+++ GoToTrainingWeek2/script1else Logical Operators.js
@@ -0,0 +1,9 @@
+//Conditional Logic - Ternary Operators
+
+var gpa = 48;
+
+//if the gpa is over the min 2.0 score, the student can graduate
+/*if( gpa > 2.0){
+
+
+ */
