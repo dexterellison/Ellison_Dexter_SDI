227753126151c554a72b87bc561fd94be6804804
dexterellison <DexterLab@Dexters-MacBook-Pro.local>
2015-09-17 20:43:39 -0700

    Fixing Problems
    Code almost error free to move on.

--- GoToTrainingWeek3/Ellison_Dexter_Conditionals_Assignment/Js/Ellison_Dexter_Conditionals_Assignment.js
+++ GoToTrainingWeek3/Ellison_Dexter_Conditionals_Assignment/Js/Ellison_Dexter_Conditionals_Assignment.js
@@ -11,31 +11,27 @@ var pairOfShoes = ["blue pair", "green pair", "black pair", "brown pair"];
 
 console.log(pairOfShoes);
 
-var totalNumBluePair = 0;
+var totalNumPairOfShoe = 0;
 
 //Which pair is the blue pair
 
 if(pairOfShoes[0]==="blue pair"){
     console.log("This is a blue pair");
-    totalNumOfBluePair++;
+
 } else {
     console.log("This is a blue pair")
 }
 if(pairOfShoes[1]==="blue pair"){
     console.log("This is not a blue pair");
-    totalNumOfBluePair++;
+
+} if(pairOfShoes[2]==="blue pair"){
+    console.log("This is a blue pair");
+
 } else {
-    console.log("This is not a blue pair");
+    console.log("This is a blue pair")
 }
-if(bowlOfFruit[2]==="blue pair"){
-    console.log("This is not a pair");
-    totalNumBluePair++;
-} else {
-    console.log("This is not a blue pair ");
-    if(pairOfShoes[3]==="blue pair"){
-        console.log("This is a blue pair");
-    }
-        totalNumBluePair++;
-    } else {
-        console.log("This is not a blue pair");
-    }
+if(pairOfShoes[3]==="blue pair"){
+    console.log("This is not a blue pair");
+
+}
+
