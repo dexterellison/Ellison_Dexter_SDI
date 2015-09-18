52ea10444e7e30e0a00d87448ccef5b979fb6a9d
dexterellison <DexterLab@Dexters-MacBook-Pro.local>
2015-09-17 20:13:38 -0700

    Number of blue shoes
    Number of blue shoes

--- GoToTrainingWeek3/Ellison_Dexter_Conditionals_Assignment/Js/Ellison_Dexter_Conditionals_Assignment.js
+++ GoToTrainingWeek3/Ellison_Dexter_Conditionals_Assignment/Js/Ellison_Dexter_Conditionals_Assignment.js
@@ -6,3 +6,41 @@
  */
 
 //alert("Testing to see if connected.");
+
+var pairOfShoes = ["blue pair", "green pair", "black pair", "brown"];
+
+console.log(pairOfShoes);
+
+var totalNumPairOfShoes = 0;
+
+//How many pairs of colored shoes I own are blue
+
+if(pairOfShoes[0]==="blue"){
+
+    console.log("This is pair of shoes blue");
+    totalNumPairOfBlueShoes++;
+} else {
+    consol.log("This is a pair of blue shoes")
+}
+
+if(pairOfShoes[1]==="blue pair"){
+
+    console.log("This is a pear");
+    totalNumPairOfBlueShoes++;
+} else {
+    consol.log("This is not a pair of blue shoes");
+}
+
+if(bowlOfFruit[2]==="blue pair"){
+
+    console.log("This is a pear");
+    totalNumPairOfBlueShoes++;
+} else {
+    consol.log("This fruit is not a pear");
+
+    if(pairOfShoes[3]==="blue pair"){
+
+        console.log("This is a pear");
+        totalNumPairOfBlueShoes++;
+    } else {
+        console.log("This is not a pair of blue shoes");
